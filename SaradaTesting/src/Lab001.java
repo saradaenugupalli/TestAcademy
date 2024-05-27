4cfa88214665231b6211ada7b0b57ea9585cf1c7
Sarada <saradaenugupalli@gmail.com>
2024-05-27 22:02:29 +0530

    this is somthing

--- SaradaTesting/src/Lab001.java
+++ SaradaTesting/src/Lab001.java
@@ -1,6 +1,8 @@
 public class Lab001 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Sarada");
+        System.out.println("Hello Ajitesh");
     }
 }
 
