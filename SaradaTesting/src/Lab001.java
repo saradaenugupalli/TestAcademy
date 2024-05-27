94a02194e503ea7e65cf61f589105907e673cfa9
Sarada <saradaenugupalli@gmail.com>
2024-05-27 21:44:14 +0530

    son

--- SaradaTesting/src/Lab001.java
+++ SaradaTesting/src/Lab001.java
@@ -1,6 +1,8 @@
 public class Lab001 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Sarda is my World");
+        System.out.println("My life is my family");
     }
 }
 
